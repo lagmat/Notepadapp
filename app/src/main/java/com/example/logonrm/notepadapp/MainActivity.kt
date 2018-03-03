45552f6efa8f9f72ec08ea092802a52413e8e8cb
logonrm <logonrm@fiap.com.br>
2018-03-03 13:44:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/notepadapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.logonrm.notepadapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override
+    fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_cadastro, menu)
+        return true
+    }
+
+    override
+    fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        when (item.getItemId()) {
+            R.id.mnPersquisar -> {
+                return true
+            }
+            R.id.mnSalvar -> {
+
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+}
